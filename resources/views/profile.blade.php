82995d0fe0951a87a0bdc7e658e7ef28967986ad
Arnon Puitrakul <peter_arnon1@icloud.com>
2017-01-25 21:10:44 +0700

    Improve Profile View with Bootstrap

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -2,13 +2,22 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="/css/app.css">
     <title>My Profile</title>
   </head>
   <body>
-    <img src="/images/profile.jpg" alt="">
-    <br>Name : Arnon Puitrakul
-    <br>Student - Faculty of ICT, Mahidol University
-    <br>Website : <a href = "https://www.arnondora.in.th">www.arnondora.in.th</a>
-    <br>MUICT#12 SB#100 MSP#17 YWC#14
+    <div class = "container">
+      <div class = "col-md-6">
+        <img class = "img-responsive" style = "max-height:500px; width:auto;" src="/images/profile.jpg" alt="">
+      </div>
+
+      <div class = "col-md-6">
+        <p>Name : Arnon Puitrakul</p>
+        <p>Student - Faculty of ICT, Mahidol University</p>
+        <p>Website : <a href = "https://www.arnondora.in.th">www.arnondora.in.th</a></p>
+        <p>MUICT#12 SB#100 MSP#17 YWC#14</p>
+      </div>
+
+    </div>
   </body>
 </html>
