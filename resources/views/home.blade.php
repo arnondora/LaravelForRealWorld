50c406555a100852cf56f7e3061a758e43c3c6df
Arnon Puitrakul <peter_arnon1@icloud.com>
2017-01-30 21:14:24 +0700

    Add Your Reminder Module

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,21 @@
 @extends('template')
 
 @section('content')
-  @include('components.NewReminder')
+  <div class = "container">
+
+    <div class = 'row'>
+      <h2>Your Reminder(s)</h2>
+
+      <div class = 'row'>
+        <div class = "well">
+          <span>Buy Milk</span>
+          <a class = "btn btn-success pull-right">Finish</a>
+        </div>
+      </div>
+
+    </div>
+
+    @include('components.NewReminder')
+
+  </div>
 @endsection
