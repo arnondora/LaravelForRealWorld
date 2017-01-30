a0581a1882238f2e22cdf31d044eda7955f3eb64
Arnon Puitrakul <peter_arnon1@icloud.com>
2017-01-30 21:19:37 +0700

    Manipulate Reminder Data

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,10 +7,13 @@
       <h2>Your Reminder(s)</h2>
 
       <div class = 'row'>
-        <div class = "well">
-          <span>Buy Milk</span>
-          <a class = "btn btn-success pull-right">Finish</a>
-        </div>
+        @foreach ($reminders as $reminder)
+          <div class = "well">
+            <span>{{$reminder}}</span>
+            <a class = "btn btn-success pull-right">Finish</a>
+          </div>
+        @endforeach
+
       </div>
 
     </div>
