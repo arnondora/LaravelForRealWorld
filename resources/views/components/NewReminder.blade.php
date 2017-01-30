6b7a3b33d3c5adab4d8f1097132febae07f126f8
Arnon Puitrakul <peter_arnon1@icloud.com>
2017-01-30 20:59:30 +0700

    Create new reminder component

---
+++ resources/views/components/NewReminder.blade.php
@@ -0,0 +1,12 @@
+<form method = "post" action = "/reminder/new">
+  <h2>New Reminder</h2>
+  <div class = "form-group">
+    <label>Reminder : </label>
+    <textarea name="reminder" rows="8" cols="80" class = "form-control"></textarea>
+  </div>
+
+  <div class = "form-group">
+    {{csrf_field()}}
+    <input class = "btn btn-primary" type="submit" value="Create New Reminder">
+  </div>
+</form>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,5 @@
 @extends('template')
 
 @section('content')
-  <form method = "post" action = "/reminder/new">
-    <h2>New Reminder</h2>
-    <div class = "form-group">
-      <label>Reminder : </label>
-      <textarea name="reminder" rows="8" cols="80" class = "form-control"></textarea>
-    </div>
-
-    <div class = "form-group">
-      {{csrf_field()}}
-      <input class = "btn btn-success" type="submit" value="Create New Reminder">
-    </div>
-  </form>
+  @include('components.NewReminder')
 @endsection
