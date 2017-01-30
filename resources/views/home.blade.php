5e9484faf465d1e6e2dca22c3f14db2f477699b3
Arnon Puitrakul <peter_arnon1@icloud.com>
2017-01-30 20:49:00 +0700

    Init Reminder App Form

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,16 @@
 @extends('template')
 
 @section('content')
-  <h3>Hello World</h3>
+  <form method = "post" action = "/reminder/new">
+    <h2>New Reminder</h2>
+    <div class = "form-group">
+      <label>Reminder : </label>
+      <textarea name="reminder" rows="8" cols="80" class = "form-control"></textarea>
+    </div>
+
+    <div class = "form-group">
+      {{csrf_field()}}
+      <input class = "btn btn-success" type="submit" value="Create New Reminder">
+    </div>
+  </form>
 @endsection
