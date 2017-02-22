ed0ff1f0b3c6d751f938d53b2ab14ba3dbb6febb
Arnon Puitrakul <peter_arnon1@icloud.com>
2017-02-22 21:07:50 +0700

    Add Profile Picture Disk

--- config/filesystems.php
+++ config/filesystems.php
@@ -54,6 +54,12 @@ return [
             'visibility' => 'public',
         ],
 
+        'profile' => [
+            'driver' => 'local',
+            'root' => storage_path('app/profile'),
+            'visibility' => 'public',
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => 'your-key',
